eaa04f712fc9f643b4b097f1508f329c0d64aa2a
github-action-benchmark <github@users.noreply.github.com>
2026-08-23 03:33:17 +0000

    add Instruction Count (iai-callgrind) (customSmallerIsBetter) benchmark result for 53cbc066608154956be7bfe7cfa31200a3ac82e5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787455623979,
+  "lastUpdate": 1787455996972,
   "repoUrl": "https://github.com/springs022/hiddenmate",
   "entries": {
     "Instruction Count (iai-callgrind)": [
@@ -21,6 +21,58 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/springs022/hiddenmate/commit/53cbc066608154956be7bfe7cfa31200a3ac82e5"
         },
         "date": 1787455619506,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "bench_black_advance",
+            "value": 50875,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_white_advance",
+            "value": 124145,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_reachable",
+            "value": 18369,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_attacker",
+            "value": 122124,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_canonicalize",
+            "value": 2258,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_near_mate",
+            "value": 435330214,
+            "unit": "Instructions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "springs022",
+            "username": "springs022",
+            "email": "hit.and.miss.masayume@gmail.com"
+          },
+          "committer": {
+            "name": "springs022",
+            "username": "springs022",
+            "email": "hit.and.miss.masayume@gmail.com"
+          },
+          "id": "53cbc066608154956be7bfe7cfa31200a3ac82e5",
+          "message": "未設定のCloud Run自動デプロイを削除",
+          "timestamp": "2026-08-23T03:07:06Z",
+          "url": "https://github.com/springs022/hiddenmate/commit/53cbc066608154956be7bfe7cfa31200a3ac82e5"
+        },
+        "date": 1787455995220,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
