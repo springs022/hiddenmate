ecac5c00c5291ba7d040ccbb8b37eb140e3fe9f6
github-action-benchmark <github@users.noreply.github.com>
2026-08-24 23:58:59 +0000

    add Instruction Count (iai-callgrind) (customSmallerIsBetter) benchmark result for 75a05342598a2e89d8895c7f1a6a8f86ff4644cc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787584478260,
+  "lastUpdate": 1787615939951,
   "repoUrl": "https://github.com/springs022/hiddenmate",
   "entries": {
     "Instruction Count (iai-callgrind)": [
@@ -801,6 +801,58 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/springs022/hiddenmate/commit/de98844ea81af184203d53dd67467fdaf72ddbc8"
         },
         "date": 1787584470224,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "bench_black_advance",
+            "value": 50875,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_white_advance",
+            "value": 124145,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_reachable",
+            "value": 18369,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_attacker",
+            "value": 122124,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_canonicalize",
+            "value": 2258,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_near_mate",
+            "value": 435330214,
+            "unit": "Instructions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "springs022",
+            "username": "springs022",
+            "email": "hit.and.miss.masayume@gmail.com"
+          },
+          "committer": {
+            "name": "springs022",
+            "username": "springs022",
+            "email": "hit.and.miss.masayume@gmail.com"
+          },
+          "id": "75a05342598a2e89d8895c7f1a6a8f86ff4644cc",
+          "message": "覆面駒の所属反転と駒台表示を修正",
+          "timestamp": "2026-08-24T23:42:57Z",
+          "url": "https://github.com/springs022/hiddenmate/commit/75a05342598a2e89d8895c7f1a6a8f86ff4644cc"
+        },
+        "date": 1787615938493,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
