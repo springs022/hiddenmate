3d576c43ea02b33474fa49e7104de2205f34ce5e
github-action-benchmark <github@users.noreply.github.com>
2026-08-24 14:41:11 +0000

    add Instruction Count (iai-callgrind) (customSmallerIsBetter) benchmark result for 420f2fc23217f80e44ac6b23f7ff5025b05f21ef

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787499276490,
+  "lastUpdate": 1787582471647,
   "repoUrl": "https://github.com/springs022/hiddenmate",
   "entries": {
     "Instruction Count (iai-callgrind)": [
@@ -697,6 +697,58 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/springs022/hiddenmate/commit/6709acf6d7a2c9c2055564a2951d2e85162a74f4"
         },
         "date": 1787499271178,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "bench_black_advance",
+            "value": 50875,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_white_advance",
+            "value": 124145,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_reachable",
+            "value": 18369,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_attacker",
+            "value": 122124,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_canonicalize",
+            "value": 2258,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_near_mate",
+            "value": 435330214,
+            "unit": "Instructions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "springs022",
+            "username": "springs022",
+            "email": "hit.and.miss.masayume@gmail.com"
+          },
+          "committer": {
+            "name": "springs022",
+            "username": "springs022",
+            "email": "hit.and.miss.masayume@gmail.com"
+          },
+          "id": "420f2fc23217f80e44ac6b23f7ff5025b05f21ef",
+          "message": "画面デザインの調整。",
+          "timestamp": "2026-08-24T14:22:33Z",
+          "url": "https://github.com/springs022/hiddenmate/commit/420f2fc23217f80e44ac6b23f7ff5025b05f21ef"
+        },
+        "date": 1787582470047,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
