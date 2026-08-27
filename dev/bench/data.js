027f4acd95f8e42ea94eb1940f3dca1f9aa9a7da
github-action-benchmark <github@users.noreply.github.com>
2026-08-27 11:29:24 +0000

    add Instruction Count (iai-callgrind) (customSmallerIsBetter) benchmark result for eba94e4b6d4f8c38c26ed62de8e5da078f31a465

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787802364015,
+  "lastUpdate": 1787830164253,
   "repoUrl": "https://github.com/springs022/hiddenmate",
   "entries": {
     "Instruction Count (iai-callgrind)": [
@@ -1789,6 +1789,58 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/springs022/hiddenmate/commit/62da160eff1d6fef34deb6367c7b6bcf4835fc88"
         },
         "date": 1787802359566,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "bench_black_advance",
+            "value": 50875,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_white_advance",
+            "value": 124145,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_reachable",
+            "value": 18369,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_attacker",
+            "value": 122124,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_canonicalize",
+            "value": 2258,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_near_mate",
+            "value": 435325904,
+            "unit": "Instructions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "springs022",
+            "username": "springs022",
+            "email": "hit.and.miss.masayume@gmail.com"
+          },
+          "committer": {
+            "name": "springs022",
+            "username": "springs022",
+            "email": "hit.and.miss.masayume@gmail.com"
+          },
+          "id": "eba94e4b6d4f8c38c26ed62de8e5da078f31a465",
+          "message": "覆面駒検討を中断可能にする",
+          "timestamp": "2026-08-27T11:11:58Z",
+          "url": "https://github.com/springs022/hiddenmate/commit/eba94e4b6d4f8c38c26ed62de8e5da078f31a465"
+        },
+        "date": 1787830162480,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
