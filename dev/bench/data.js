c638b6dcfd7f1f2c7f78693977e4fc98476aa4af
github-action-benchmark <github@users.noreply.github.com>
2026-08-29 02:59:04 +0000

    add Instruction Count (iai-callgrind) (customSmallerIsBetter) benchmark result for 08721edc816477dc53b0617dec81443e41c51405

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787971297711,
+  "lastUpdate": 1787972344738,
   "repoUrl": "https://github.com/springs022/hiddenmate",
   "entries": {
     "Instruction Count (iai-callgrind)": [
@@ -2569,6 +2569,58 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/springs022/hiddenmate/commit/08721edc816477dc53b0617dec81443e41c51405"
         },
         "date": 1787971293920,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "bench_black_advance",
+            "value": 50875,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_white_advance",
+            "value": 124145,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_reachable",
+            "value": 18369,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_attacker",
+            "value": 122124,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_canonicalize",
+            "value": 2258,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_near_mate",
+            "value": 435325904,
+            "unit": "Instructions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "springs022",
+            "username": "springs022",
+            "email": "hit.and.miss.masayume@gmail.com"
+          },
+          "committer": {
+            "name": "springs022",
+            "username": "springs022",
+            "email": "hit.and.miss.masayume@gmail.com"
+          },
+          "id": "08721edc816477dc53b0617dec81443e41c51405",
+          "message": "PC表示の検討結果欄に余白を追加",
+          "timestamp": "2026-08-29T02:38:22Z",
+          "url": "https://github.com/springs022/hiddenmate/commit/08721edc816477dc53b0617dec81443e41c51405"
+        },
+        "date": 1787972343568,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
