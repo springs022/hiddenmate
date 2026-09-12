2b6248290bdc21807474c77e0a2e81d3f277de58
github-action-benchmark <github@users.noreply.github.com>
2026-09-12 03:12:00 +0000

    add Instruction Count (iai-callgrind) (customSmallerIsBetter) benchmark result for 65ab2bbae4e4d26600a7239868e543027190211f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788595209990,
+  "lastUpdate": 1789182720086,
   "repoUrl": "https://github.com/springs022/hiddenmate",
   "entries": {
     "Instruction Count (iai-callgrind)": [
@@ -3557,6 +3557,58 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/springs022/hiddenmate/commit/10b31fbf18a640fb5870c1ba60e3a44c403cfce6"
         },
         "date": 1788595204778,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "bench_black_advance",
+            "value": 50875,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_white_advance",
+            "value": 124145,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_reachable",
+            "value": 18369,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_attacker",
+            "value": 122124,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_canonicalize",
+            "value": 2258,
+            "unit": "Instructions"
+          },
+          {
+            "name": "bench_near_mate",
+            "value": 435325904,
+            "unit": "Instructions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "springs022",
+            "username": "springs022",
+            "email": "hit.and.miss.masayume@gmail.com"
+          },
+          "committer": {
+            "name": "springs022",
+            "username": "springs022",
+            "email": "hit.and.miss.masayume@gmail.com"
+          },
+          "id": "65ab2bbae4e4d26600a7239868e543027190211f",
+          "message": "透明駒の最善詰に対応",
+          "timestamp": "2026-09-12T02:51:21Z",
+          "url": "https://github.com/springs022/hiddenmate/commit/65ab2bbae4e4d26600a7239868e543027190211f"
+        },
+        "date": 1789182718428,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
